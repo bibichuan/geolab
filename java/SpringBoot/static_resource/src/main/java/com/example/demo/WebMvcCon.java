bb97f37d3fdf18ebea442063642a0b0905ea5710
phzlc <741841403@qq.com>
2020-05-19 19:06:44 +0800

    修改静态资源映射方法

--- java/SpringBoot/static_resource/src/main/java/com/example/demo/WebMvcCon.java
+++ java/SpringBoot/static_resource/src/main/java/com/example/demo/WebMvcCon.java
@@ -9,24 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcCon implements WebMvcConfigurer {
 
-  @Override
-    public void addCorsMappings(CorsRegistry registry) {
-//       registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowCredentials(true)
-//                .allowedMethods("GET", "POST", "DELETE", "PUT","PATCH")
-//                .maxAge(3600);
-//        registry.addMapping("/**").allowCredentials(true);
-
-    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //registry.addResourceHandler("/upload/**").addResourceLocations("classpath:/upload/");
-//        registry.addResourceHandler("/upload/**").addResourceLocations("file:"+uploadImgUrl+"/");
-//        registry.addResourceHandler("/upload/**").addResourceLocations("file:"+signatureImgUrl+"/");
-//
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/META-INF/resources/", "classpath:/static/css", "/static/css", "/public");
-//
-//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//        registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/",
+//                "classpath:/resources/static/css/", "/static/css", "/public","/static");
+
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/META-INF/resources/").addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/css/").addResourceLocations("classpath:/public/");
     }
 }
